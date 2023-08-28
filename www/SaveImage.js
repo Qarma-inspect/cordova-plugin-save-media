b87d947a45cc032e086d2386a11df67b15dec727
Chris Khong <chris.khong@qarmainspect.com>
2023-08-28 10:37:10 +0200

    refactor

--- www/SaveImage.js
+++ www/SaveImage.js
@@ -1,14 +1,14 @@
 exports.saveImageToGallery = function (localImagePath, title, successCallback, failureCallback) {
     if (typeof successCallback != 'function') {
-        throw new Error('SaveImage Error: successCallback is not a function');
+        throw new Error('ImageSaver Error: successCallback is not a function');
     }
 
     if (typeof failureCallback != 'function') {
-        throw new Error('SaveImage Error: failureCallback is not a function');
+        throw new Error('ImageSaver Error: failureCallback is not a function');
     }
 
     return cordova.exec(
-        successCallback, failureCallback, 'SaveImage', 'saveImageToGallery', [_getLocalImagePathWithoutPrefix(), title]);
+        successCallback, failureCallback, 'ImageSaver', 'saveImageToGallery', [_getLocalImagePathWithoutPrefix(), title]);
 
     function _getLocalImagePathWithoutPrefix() {
         if (localImagePath.indexOf('file:///') === 0) {
@@ -20,15 +20,15 @@ exports.saveImageToGallery = function (localImagePath, title, successCallback, f
 
 exports.saveVideoToGallery = function (localVideoPath, title, successCallback, failureCallback) {
     if (typeof successCallback != 'function') {
-        throw new Error('SaveImage Error: successCallback is not a function');
+        throw new Error('ImageSaver Error: successCallback is not a function');
     }
 
     if (typeof failureCallback != 'function') {
-        throw new Error('SaveImage Error: failureCallback is not a function');
+        throw new Error('ImageSaver Error: failureCallback is not a function');
     }
 
     return cordova.exec(
-        successCallback, failureCallback, 'SaveImage', 'saveVideoToGallery', [_getLocalImagePathWithoutPrefix(), title]);
+        successCallback, failureCallback, 'ImageSaver', 'saveVideoToGallery', [_getLocalImagePathWithoutPrefix(), title]);
 
     function _getLocalImagePathWithoutPrefix() {
         if (localVideoPath.indexOf('file:///') === 0) {
